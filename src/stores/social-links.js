16679777844b015812a72a21181694d067068246
Azimxan <azimabdulhanov88@gmail.com>
2024-09-05 11:22:36 +0500

    fixed bag icons

--- src/stores/social-links.js
+++ src/stores/social-links.js
@@ -6,25 +6,25 @@ export const useSocialLinks = defineStore('socialLinks', {
 			{
 				id: 1,
 				name: '@azimxan',
-				icon: '/public/images/telegram.svg',
+				icon: '/images/telegram.svg',
 				url: 'https://t.me/azimkhan_abdulkhanov'
 			},
 			{ 
 				id: 2,
 				name: '@abdulkhanov__a', 
-				icon: '/public/images/instagram.svg',
+				icon: '/images/instagram.svg',
 				url: 'https://www.instagram.com/abdulkhanov__a/' 
 			},
 			{ 
 				id: 3,
 				name: 'azimkhan-abdulkhanov', 
-				icon: '/public/images/linkedin.svg',
+				icon: '/images/linkedin.svg',
 				url: 'https://www.linkedin.com/in/azimkhan-abdulkhanov-6ab0b6314?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' 
 			},
 			{
 				id: 4,
 				name: 'azim-abdulhanov', 
-				icon: '/public/images/github.svg',
+				icon: '/images/github.svg',
 				url: 'https://github.com/azim-abdulhanov' 
 			},
 		]
